2f3ae7f1dff9116a45b867c336905ce0193fae24
vaastav <vaastav.anand05@gmail.com>
2016-02-13 09:49:08 -0800

    Update the Activity to setup a simple UI

--- app/src/main/java/com/vaastavanand/rssfeed/RSSReaderActivity.java
+++ app/src/main/java/com/vaastavanand/rssfeed/RSSReaderActivity.java
@@ -1,70 +1,19 @@
 package com.vaastavanand.rssfeed;
 
 import android.app.ListActivity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.vaastavanand.rssfeed.data.RssItem;
-import com.vaastavanand.rssfeed.listeners.ListListener;
-import com.vaastavanand.rssfeed.util.RssReader;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class RSSReaderActivity extends ListActivity {
 
-    private RSSReaderActivity local;
+    private String[] listData = new String[]{"Post 1","Post 2","Post 3", "Post 4","Post 5","Post 6"};
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postlist);
-        local = this;
-        GetRSSDataTask task = new GetRSSDataTask();
-        task.execute("http://www.nasa.gov/rss/dyn/solar_system.rss");
-        Log.d("RSS_ACT",Thread.currentThread().getName());
+        ListView listView = (ListView)this.findViewById( R.id.listView );
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>( this,R.layout.postitem,listData );
+        listView.setAdapter( itemAdapter );
     }
-
-    private class GetRSSDataTask extends AsyncTask<String,Void,List<RssItem> >{
-
-        @Override
-        protected List<RssItem> doInBackground(String... params) {
-
-            Log.d("RSS_FEED",Thread.currentThread().getName());
-
-            try{
-                RssReader rssReader = new RssReader(params[0]);
-                return rssReader.getItems();
-            }catch(Exception e){
-                Log.e("RSS_READER",e.getMessage());
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(List<RssItem> result){
-            ListView itcItems = (ListView) findViewById(R.id.list_item);
-            ArrayAdapter<RssItem> adapter = new ArrayAdapter<RssItem>(local,android.R.layout.simple_list_item_1,result);
-            itcItems.setAdapter(adapter);
-            itcItems.setOnItemClickListener(new ListListener(result,local));
-        }
-    }
-
 }
