5eb08151323abef84d3ad8e7e082d6e357cf608a
vaastav <vaastav.anand05@gmail.com>
2016-02-13 10:26:58 -0800

    Fix java.lang.RuntimeException: Your content must have a ListView whose id attribute is android.R.id.list
    NOTE: Never extend ListActivity. Extending Activity is better

--- app/src/main/java/com/vaastavanand/rssfeed/RSSReaderActivity.java
+++ app/src/main/java/com/vaastavanand/rssfeed/RSSReaderActivity.java
@@ -1,18 +1,18 @@
 package com.vaastavanand.rssfeed;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class RSSReaderActivity extends ListActivity {
+public class RSSReaderActivity extends Activity {
 
     private String[] listData = new String[]{"Post 1","Post 2","Post 3", "Post 4","Post 5","Post 6"};
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postlist);
-        ListView listView = (ListView)this.findViewById( R.id.listView );
+        ListView listView = (ListView)this.findViewById( R.id.list );
         ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>( this,R.layout.postitem,listData );
         listView.setAdapter( itemAdapter );
     }
